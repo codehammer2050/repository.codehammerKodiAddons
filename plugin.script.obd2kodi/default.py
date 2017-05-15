4b0b543b079cfcc286d3d57dfae517a66e1b6961
codehammer2050 <alexanderschneider@MacBook-Pro.local>
2017-05-15 13:19:34 +0200

    fix

--- plugin.script.obd2kodi/default.py
+++ plugin.script.obd2kodi/default.py
@@ -2,6 +2,17 @@ import xbmcaddon
 import xbmcgui
 import os
 from src import piusv
+from __future__ import print_function
+
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+logger = logging.getLogger()
+logger.addHandler(logging.FileHandler('test.log', 'a'))
+print = logger.info
+
+print('yo!')
+
 
 
 piusv = piusv.PiUSV()
